63c5e25ffe677b1b9e62e4717bbecc7c2f14a01e
najongjine <najongjin3@hotmail.com>
2026-06-02 10:49:50 +0900

    .

--- src/app/MemoEditScreen.tsx
+++ src/app/MemoEditScreen.tsx
@@ -1,11 +1,15 @@
+import { useLocalSearchParams } from "expo-router";
 import { ScrollView, StyleSheet, Text } from "react-native";
 
 import { View } from "react-native";
 
 export default function MemoEditScreen() {
+  const params = useLocalSearchParams();
+  const id = Number(params?.id || 0);
   return (
     <ScrollView>
       <View>
+        <Text>id:{id}</Text>
         <Text>메모작성</Text>
       </View>
     </ScrollView>
