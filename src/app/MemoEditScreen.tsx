af99fdd2e6cf0f9626aeb8d1f1d474c49ac86025
najongjine <najongjin3@hotmail.com>
2026-06-02 10:36:24 +0900

    .

---
+++ src/app/MemoEditScreen.tsx
@@ -0,0 +1,15 @@
+import { ScrollView, StyleSheet, Text } from "react-native";
+
+import { View } from "react-native";
+
+export default function MemoEditScreen() {
+  return (
+    <ScrollView>
+      <View>
+        <Text>메모작성</Text>
+      </View>
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({});
